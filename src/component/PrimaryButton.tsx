53899be76399178cb43a7861a8025474859c7a5b
shawonahme <shawonahmed55660@gmail.com>
2025-06-20 01:17:45 +0600

    first commit

---
+++ src/component/PrimaryButton.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+interface PrimaryButtonProps{
+    logo:React.ReactNode,
+    text:string
+}
+
+const PrimaryButton:React.FC<PrimaryButtonProps> = ({logo,text}) => {
+    return (
+        <div>
+            <button className='border-[1px] flex items-center gap-3 py-3 pr-3 rounded-full '><span className='rounded-full flex items-center py-2 px-2 border-[1px]'>{logo}</span> <span>{text}</span></button>
+        </div>
+    );
+};
+
+export default PrimaryButton;
